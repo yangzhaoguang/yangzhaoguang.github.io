ef506503b73a88d7b81169d26355b9e401497cbe
“杨照光” <yangzhaoguang@ciictec.com>
2024-03-22 10:24:14 +0800

    搜索插件

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,21 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
+
 export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "鲨瓜",
+  description: "",
 
   theme,
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
+
+  head: [
+    ['meta', { name: 'referrer', content: 'no-referrer' }]
+  ],
+
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -29,6 +29,8 @@ export default hopeTheme({
   footer: "默认页脚",
   displayFooter: true,
 
+
+
   // 博客相关
   blog: {
     description: "一个前端开发者",
@@ -86,14 +88,30 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
+
+    searchPro:{
+      indexContent:true,
+      hotReload:true,
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category as any,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag as any,
+          formatter: "标签：$content",
+        },
+      ],
+    },
     blog: true,
+
 
     // 在启用之前需要安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务器，在生产环境中请自行部署并使用自己的服务器！
-    // comment: {
-    //   provider: "Waline",
-    //   serverURL: "https://waline-comment.vuejs.press",
-    // },
+    comment: {
+      provider: "Waline",
+      serverURL: "https://waline-comment.vuejs.press",
+    },
 
     components: {
       components: ["Badge", "VPCard"],
